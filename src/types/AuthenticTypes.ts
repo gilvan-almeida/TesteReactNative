8bd7e21e3a7ebdcc24f08e38d8f097e23d06a454
gilvan_almeida <gilvanalmeidaaraujo748@gmail.com>
2026-06-07 19:07:58 -0300

    feat(auth):implement passwordPin authentication fallback

--- src/types/AuthenticTypes.ts
+++ src/types/AuthenticTypes.ts
@@ -7,3 +7,12 @@ export interface BackgroundLockType{
     verifyAuthentic: boolean;
     onLock: () => void;
 }
+
+export interface UseAuthPinType{
+    onSuccess: () => void;
+}
+
+export interface PinInputType {
+    pin: string;
+    onChangePin: (value: string) => void;
+}
